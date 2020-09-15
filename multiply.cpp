75a99f97fae74517c5fa2e2ae59bf4b65ddb0d7a
ditave <55725166+ditave@users.noreply.github.com>
2020-09-15 13:05:23 +0800

    Create multiply.cpp
    迭代版的简单乘法

---
+++ multiply.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+using namespace std;
+
+int multiply(int a, int b)
+{
+	int sum = 0;
+	int sign = 0;
+	if (b < 0)
+	{
+		b = 0 - b;
+		sign = 1;
+	}
+
+	while (b)
+	{
+		if (b & 1)
+			sum += a;
+		a = a << 1;
+		b = b >> 1;
+	}
+	if (sign == 1)
+		return -sum;
+	else
+		return sum;
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << multiply(a, b) << endl;
+}
